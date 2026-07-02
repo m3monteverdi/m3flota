9ed99894e2e37af2dfd85711ef05fe485a32ecdf
m3monteverdi <m3monteverdi@gmail.com>
2026-07-02 14:46:39 -0300

    m3flota: elimina codigo duplicado/incompleto en registrarPreventivo() que causaba SyntaxError

--- js/app.js
+++ js/app.js
@@ -231,7 +231,9 @@ function registrarPreventivo() {
       setTimeout(registrarPreventivo, 100);
     }
   }, 200);
-}function showMsg(id, type, txt) {
+}
+
+function showMsg(id, type, txt) {
   var el = document.getElementById(id);
   el.querySelector('span').textContent = txt;
   el.className = 'msg ' + (type === 'ok' ? 'mok' : 'merr');
